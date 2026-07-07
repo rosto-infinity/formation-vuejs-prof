dffce832457d805bee8d59b83223dfadbf7a68f4
rosto-infinity <rostoinfocus@gmail.com>
2026-07-07 15:16:37 +0100

    v-bind en css

--- src/components/MessageRendering.vue
+++ src/components/MessageRendering.vue
@@ -1,9 +1,18 @@
 <script setup>
-import {ref, computed} from 'vue';
+import { computed, ref } from 'vue';
 
+// État réactif (State)
 const msg = ref('Mon apprentissage en vue js');
 const count = ref(0);
+
+//v-bind en css
+ const color = ref('red')
+ const fontSize =ref(16)
+
+ // Propriétés calculées (Computed)
 const doubleCount = computed(() => count.value * 2);
+
+// Fonctions classiques
 function increment(){
   count.value++;
 }
@@ -12,11 +21,14 @@ function increment(){
 </script>
 
 <template>
-    <p class="text"> Voir : {{ msg }} </p>
+    <p class="text1"> Voir : {{ msg }} </p>
     <p>Compteur : {{ count }}</p>
     <p>Compteur : {{ doubleCount }}</p>
     <button @click="increment">Incrementer</button>
     
+
+    <p class="text">Design totalement dynamique !</p>
+    <button @click="color = 'blue'; fontSize = '23'">Passer au Bleu</button>
 </template>
 
 <style scoped>
@@ -26,7 +38,7 @@ h1 {
   position: relative;
   top: -10px;
 }
-.text{
+.text1{
   color:rgb(3, 199, 3);
   font-size: 2rem;
 }
@@ -40,6 +52,12 @@ h3 {
   text-align: center;
 }
 
+
+  .text {
+    /* Le CSS se mettra à jour en temps réel si la variable color change dans le JS ! */
+    color: v-bind(color);
+    font-size: v-bind(fontSize + 'px');
+}
 @media (min-width: 1024px) {
   .greetings h1,
   .greetings h3 {
