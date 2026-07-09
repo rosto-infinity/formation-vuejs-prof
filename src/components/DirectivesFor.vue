2bfbb8a3c2b47f77fb4cd86a93cfa585e0b39231
rosto-infinity <rostoinfocus@gmail.com>
2026-07-09 19:30:09 +0100

    EXCELLENT : Un ID unique venant de la BDD comme clé

--- src/components/DirectivesFor.vue
+++ src/components/DirectivesFor.vue
@@ -17,6 +17,25 @@ const users = ref([
   { id: 2, name: "Bob", email: "bob@exemple.com" },
   { id: 3, name: "Charlie", email: "charlie@exemple.com" },
 ]);
+
+const categories = ref([
+  {
+    id: 1,
+    name: "Électronique",
+    products: [
+      { id: 101, name: "Téléphone" },
+      { id: 102, name: "PC Portable" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Vêtements",
+    products: [
+      { id: 201, name: "T-Shirt" },
+      { id: 202, name: "Jeans" },
+    ],
+  },
+]);
 </script>
 
 <template>
@@ -33,7 +52,7 @@ const users = ref([
 </ul>
 
 
- <!-- EXCELLENT : Un ID unique venant de la BDD comme clé -->
+ <!-- --EXCELLENT : Un ID unique venant de la BDD comme clé -->
   <div v-for="user in users" :key="user.id">
     {{ user.name }} - {{ user.email }}
   </div>
@@ -41,5 +60,25 @@ const users = ref([
   <!-- MAUVAIS ACTE : L'index comme clé (Cause de gros bugs lors des tris) -->
   <div v-for="(user, index) in users" :key="index">
     {{ user.name }}
+  </div>
+
+  <!-- #Le v-for sur une Plage de Nombres (Range) -->
+
+  <!-- Affiche 1, 2, 3, 4, 5 -->
+  <span v-for="n in 5" :key="n">{{ n }}</span>
+
+  <!-- Pratique pour créer 10 fausses cartes (Skeleton Loaders) -->
+  <div v-for="i in 10" :key="i" class="skeleton-item">
+    Chargement élément {{ i }}...
+  </div>
+
+  <div v-for="category in categories" :key="category.id">
+    <h3>{{ category.name }}</h3>
+    <ul>
+      <!-- Le v-for à l'intérieur utilise la variable `category` du v-for parent ! -->
+      <li v-for="product in category.products" :key="product.id">
+        {{ product.name }}
+      </li>
+    </ul>
   </div>
 </template>
