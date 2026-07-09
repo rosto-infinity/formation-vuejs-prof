6365b125ab5a8bca932e9192e7fd1246af215273
rosto-infinity <rostoinfocus@gmail.com>
2026-07-09 19:24:51 +0100

    EXCELLENT : Un ID unique venant de la BDD comme clé

--- src/components/DirectivesFor.vue
+++ src/components/DirectivesFor.vue
@@ -9,6 +9,14 @@ const tasks = ref([
   { id: 2, text: "Créer une appli", done: false },
   { id: 3, text: "Déployer en production", done: false },
 ]);
+
+// L'Attribut key - Absolument Critique !
+
+const users = ref([
+  { id: 1, name: "Alice", email: "alice@exemple.com" },
+  { id: 2, name: "Bob", email: "bob@exemple.com" },
+  { id: 3, name: "Charlie", email: "charlie@exemple.com" },
+]);
 </script>
 
 <template>
@@ -23,4 +31,15 @@ const tasks = ref([
    <span v-if="task.done"></span>
  </li>
 </ul>
+
+
+ <!-- EXCELLENT : Un ID unique venant de la BDD comme clé -->
+  <div v-for="user in users" :key="user.id">
+    {{ user.name }} - {{ user.email }}
+  </div>
+
+  <!-- MAUVAIS ACTE : L'index comme clé (Cause de gros bugs lors des tris) -->
+  <div v-for="(user, index) in users" :key="index">
+    {{ user.name }}
+  </div>
 </template>
